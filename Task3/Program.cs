a19917ed99bd5868ef8674eb774d5f5e93652f8a
Sergei Klokov <s2584835@ya.ru>
2024-02-04 22:16:39 +0300

    feat: solve task3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+namespace task3
+{
+    class Task3
+    {
+        static void ShowArray(int[] nums, int n)
+        {
+            if (n == 0)
+            {
+                Console.Write($"{nums[n]}");
+                return;
+            }
+            Console.Write($"{nums[n]}, ");
+            ShowArray(nums, --n);
+        }
+        
+        
+
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 3, 5, 7, 8, 10, 5, 3};
+            ShowArray(nums, nums.Length - 1);
+        }
+    }
+}
