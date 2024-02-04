6672ea2fd6563875b7edd92e2ef7812012b43474
Sergei Klokov <s2584835@ya.ru>
2024-02-04 12:15:01 +0300

    check mistake

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+namespace task1
+{
+    class Task1
+    {
+        static void RecursionPrintNumber(int fNumber, int sNumber)
+        {
+            Console.Write(fNumber);
+            if (fNumber == sNumber) return;
+            RecursionPrintNumber(++fNumber, sNumber);
+        }
+
+        static bool CheckFirstAndSecondNumbers(ref int fNumber, ref int sNumber)
+        {
+            if (fNumber <= sNumber) return true;
+            else
+            {
+                int temp;
+                temp = fNumber;
+                fNumber = sNumber;
+                sNumber = temp;
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число начала числового ряда: ");
+            int firstNumber = int.Parse(Console.ReadLine()!);
+            Console.Write("Введите натуральное число конца числового ряда: ");
+            int secondNumber = int.Parse(Console.ReadLine()!);
+            if (!CheckFirstAndSecondNumbers(ref firstNumber, ref secondNumber)) Console.WriteLine("Последовательность ваших чисел заменил.");
+
+            RecursionPrintNumber(firstNumber, secondNumber);
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+    // Если m = 0, то A(m, n) = n + 1
+    // Если m > 0 и n = 0, то A(m, n) = A(m - 1, 1)
+    // Если m > 0 и n > 0, то A(m, n) = A(m - 1, A(m, n - 1))
+
+namespace task2
+{
+    class Task2
+    {
+        static int AckermanFunctions(int m, int n)
+        {
+            if (m == 0) return n + 1;
+            else if (m > 0 && n == 0) return AckermanFunctions(m - 1, 1);
+            else return AckermanFunctions(m - 1, AckermanFunctions(m, n - 1));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число для расчет функции Аккермана: ");
+            int firstNumber = int.Parse(Console.ReadLine()!);
+            Console.Write("Введите второе число для расчет функции Аккермана: ");
+            int secondNumber = int.Parse(Console.ReadLine()!);
+
+            Console.Write(AckermanFunctions(firstNumber, secondNumber));
+        }
+
+    }
+}
